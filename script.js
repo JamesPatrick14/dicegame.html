587895321727503249d27f634d0750ded7122418
JamesPatrick14 <71359468+JamesPatrick14@users.noreply.github.com>
2021-01-17 19:59:27 +0800

    Create script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const firstRandomNum = Math.floor(Math.random()*6) + 1
 
-const firstDiceImage= 'images/dice' + firstRandomNum +'.png';
+const firstDiceImage= 'dice' + firstRandomNum + '.png';
 
 document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
 
 const secondRandomNum = Math.floor(Math.random()*6) + 1
 
-const secondDiceImage= 'images/dice' + secondRandomNum + '.png';
+const secondDiceImage= 'dice' + secondRandomNum + '.png';
 
 document.querySelectorAll('img')[1].setAttribute('src',secondDiceImage);
 
